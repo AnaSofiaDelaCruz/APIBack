26aace0c174ecfe11a19972bee605131b71d5aeb
AnaSofiaDelaCruz <anasofia_navarro@hotmail.com>
2022-12-07 11:50:17 -0600

    cors2

--- app.js
+++ app.js
@@ -9,8 +9,9 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-const cors = require("cors");
+const cors = require('cors');
 
+app.use(cors())
 
 app.use(bodyParser.json());
 
@@ -25,9 +26,9 @@ app.use(bodyParser.json());
 
 
 
-app.use(cors())
 
-app.use((req, res, next) => {
+
+app.use(function(req, res, next){
     res.header("Access-Control-Allow-Origin", "*");
     res.header(
       "Access-Control-Allow-Headers",
@@ -36,7 +37,7 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
     res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
     next();
-  });
+});
 
 
 //Route
